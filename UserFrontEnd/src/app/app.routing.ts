39884b4307b90855fd364e96e2517bdcdda8062b
Dawei <dawei.zhuang@antra.com>
2021-11-03 16:27:36 -0400

    Added:  improve UI

--- UserFrontEnd/src/app/app.routing.ts
+++ UserFrontEnd/src/app/app.routing.ts
@@ -4,9 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule, Routes} from '@angular/router';
 
 import {ComponentsComponent} from './components/components.component';
-import {ProfileComponent} from './examples/profile/profile.component';
 import {SignupComponent} from './components/signup/signup.component';
-import {LandingComponent} from './examples/landing/landing.component';
 import {NucleoiconsComponent} from './components/nucleoicons/nucleoicons.component';
 import {SigninComponent} from "./components/signin/signin.component";
 import {ControlBoardComponent} from "./components/control-board/control-board.component";
@@ -14,10 +12,8 @@ import {ControlBoardComponent} from "./components/control-board/control-board.co
 const routes: Routes =[
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home',             component: ComponentsComponent },
-    { path: 'user-profile',     component: ProfileComponent },
     { path: 'signup',           component: SignupComponent },
     { path: 'login',           component: SigninComponent },
-    { path: 'landing',          component: LandingComponent },
     { path: 'nucleoicons',      component: NucleoiconsComponent },
     { path: 'control',      component: ControlBoardComponent }
 ];
